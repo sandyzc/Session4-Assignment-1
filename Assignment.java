2ea5a89e4dca3bc792cfd0c3344a79f30abb9cf0
santosh s c <san.461@gmail.com>
2016-11-13 00:43:06 +0530

    assignment 1

---
+++ Assignment.java
@@ -0,0 +1,15 @@
+package package2;
+
+import package1.Assignment1;
+
+public class Assignment extends Assignment1 {
+	
+	public static void main(String[] args) {
+		
+	
+	Assignment as = new Assignment();
+	as.methodOne();
+	
+	}
+
+}
---
+++ Assignment1.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class Assignment1 {
+	
+	protected void methodOne(){
+		System.out.println("method from diffrent package using protected access modifier called ");
+	}
+
+}
